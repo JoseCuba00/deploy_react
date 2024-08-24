ea82f5b177be1aa3ae2d68275507645c7ab61d19
Jose Carlos <jose@email.com>
2024-08-24 11:24:40 +0200

    con variables de entorno y base de datos en choreo

--- lms_api/settings.py
+++ lms_api/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@yl52cg(y=lcxr%a&=-ae2w8*gp*am-zdx6!iel5=5xjl(vg(%"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -93,12 +97,12 @@ REST_FRAMEWORK = { # Para indicar que la autentificacion va a ser por token
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "railway",
-       "USER":"root",
-       "PASSWORD":"WstxgtvVVlRzSrVmdlNgHGjIhhbDOwUa",
-       "HOST":"mysql.railway.internal",
-       "PORT":"3306",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME":os.getenv("DB_NAME") ,
+       "USER":os.getenv("DB_USER"),
+       "PASSWORD":os.getenv("DB_PWD"),
+       "HOST":os.getenv("DB_HOST"),
+       "PORT":os.getenv("DB_PORT"),
     }
 }
 
